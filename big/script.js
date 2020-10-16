8b1f5331ab47e14995599e1f5619b1bb147155cd
AmerOmari <Cobra.amer18@gmail.com>
2020-10-16 03:12:21 +0300

    wow

--- big/script.js
+++ big/script.js
@@ -1,16 +1,19 @@
 
+
+var  logo = document.querySelector(".Mylogo"),
+ trigger = document.querySelector('.hamburger'),
+ wholeConent = document.querySelector('#page-content-wrapper');
 //sidebar work
+
 $(document).ready(function () {
-  var trigger = document.querySelector('.hamburger'),
-      overlay = document.querySelector('.overlay'),
+
+    var  overlay = document.querySelector('.overlay'),
+      wholebody = document.getElementsByTagName("BODY")[0],
+      wrapper = document.querySelector('#wrapper'),
      isClosed = false;
-
-
 
     trigger.click(function () {
       Sidebar_cross();
-
-
     });
 
     function Sidebar_cross() {
@@ -22,34 +25,40 @@ $(document).ready(function () {
         trigger.addClass('is-closed');
         isClosed = false;
 
-
       } else {
         overlay.show();
         trigger.removeClass('is-closed');
         trigger.addClass('is-open');
         isClosed = true;
-         console.log(isClosed);
       }
   }
 
-
-
-
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
+
   });
+
+  //**make the sidebar close when click anywhere on the conent***////
+  function closeNav(e){
+    e.preventDefault();
+    wrapper.classList.remove("toggled");
+  };
+   wholeConent.addEventListener("click",closeNav,false);
+  /**end of sidebar close*///
+
 });
 //sidebar end
+
+
 // Make the logo disapear once the nave open
-var  logo = document.querySelector(".Mylogo");
-var trigger = document.querySelector('.hamburger');
-
-
-
 function getLogo(e) {
     e.preventDefault();
     logo.classList.toggle("hide");
 }
+function retrivelogo(q) {
+    q.preventDefault();
+    logo.classList.remove("hide");
+}
 trigger.addEventListener("click",getLogo,false);
-
+   wholeConent.addEventListener("click",retrivelogo,false);
 //done with the logo disapearing
