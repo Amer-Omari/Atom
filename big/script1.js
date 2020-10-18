ccd4a70ece70caa11d71f6d6a874ef146f336d75
AmerOmari <Cobra.amer18@gmail.com>
2020-10-18 21:31:26 +0300

    new

--- big/script1.js
+++ big/script1.js
@@ -3,14 +3,18 @@
 var  logo = document.querySelector(".Mylogo"),
  trigger = document.querySelector('.hamburger'),
  mainPageContent=document.querySelector("#mainPageContent"),
- wholeConent = document.querySelector('#page-content-wrapper');
+ wholeConent = document.querySelector('#page-content-wrapper'),
+ footer = document.querySelector(".page-footer");
+
 //sidebar work
 
 $(document).ready(function () {
 
     var  overlay = document.querySelector('.overlay'),
+
       wholebody = document.getElementsByTagName("BODY")[0],
       wrapper = document.querySelector('#wrapper'),
+      wrapperFooter = document.querySelector('#wrapper-footer'),
      isClosed = false;
 
     trigger.click(function () {
@@ -22,20 +26,27 @@ $(document).ready(function () {
       if (isClosed == true) {
 
         overlay.hide();
+
         trigger.removeClass('is-open');
         trigger.addClass('is-closed');
         isClosed = false;
+
 
       } else {
         overlay.show();
         trigger.removeClass('is-closed');
         trigger.addClass('is-open');
         isClosed = true;
+
       }
   }
 
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
+          $('#wrapper-footer').toggleClass('toggled');
+
+
+
 
   });
 
@@ -47,6 +58,7 @@ $(document).ready(function () {
   };
    wholeConent.addEventListener("click",closeNav,false);
    mainPageContent.addEventListener("click",closeNav,false);
+    footer.addEventListener("click",closeNav,false);
   /**end of sidebar close*///
 
   //when you scroll down
@@ -76,4 +88,6 @@ function retrivelogo(q) {
 }
 trigger.addEventListener("click",getLogo,false);
    wholeConent.addEventListener("click",retrivelogo,false);
+  mainPageContent.addEventListener("click",retrivelogo,false);
+  footer.addEventListener("click",retrivelogo,false);
 //done with the logo disapearing
